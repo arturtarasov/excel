d94996c2e6976bad66cb7c16516147fc8c053f41
arturtarasov <artur-tarasov@mail.ru>
2020-09-26 12:04:10 +0300

    create methods to add html

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import { Excel } from './components/excel/excel'
+import { Formula } from './components/formula/Formula'
+import { Header } from './components/header/Header'
+import { Table } from './components/table/Table'
+import { Toolbar } from './components/toolbar/Toolbar'
 import './scss/index.scss'
 
 const excel = new Excel('#app', {
-    components: []
+    components: [Header, Toolbar, Formula, Table]
 })
-console.log('\n EXCEL ', excel)
+excel.render()
+console.log('\n EXCEL ', excel.$el)
